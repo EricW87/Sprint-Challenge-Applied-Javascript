6efcb368310c99288b1789df76bb363efde0e9fb
U-steam-PC\steam <EricW87@github.com>
2019-10-13 16:29:22 -0700

    Added Header/index.js.

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,28 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+	const divElement = document.createElement('div');
+	divElement.classList.add('header');
+
+	const spanDateElement = document.createElement('span');
+	spanDateElement.classList.add('date');
+	spanDateElement.textContent = "SMARCH 28, 2019";
+
+	const h1Element = document.createElement('h1');
+	h1Element.textContent = "Lambda Times";
+
+	const spanElement = document.createElement('span');
+	spanElement.classList.add('temp');
+	spanElement.textContent = "98*";
+
+	divElement.appendChild(spanDateElement);
+	divElement.appendChild(h1Element);
+	divElement.appendChild(spanElement);
+
+	return divElement;
+}
+
+
+const headerContainer = document.querySelector('.header-container');
+headerContainer.appendChild(new Header());
